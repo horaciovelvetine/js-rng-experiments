115c7a29132813ca367f25eb429f6a8c13261929
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2023-02-13 13:02:19 -0700

    finds all results with mean value of occurences

--- src/func/mean-plot.ts
+++ src/func/mean-plot.ts
@@ -6,8 +6,7 @@ type DICE_POINTS = DICE_DATA_POINT | DICE_DATA_POINT[];
 export function generateMeanPlotData(dataSet: DICE_DATA_POINT[]): SDC_MEAN_CHART_PROPS {
 	const minimum: DICE_POINTS = findMinimum(dataSet);
 	const lowerQuartile: DICE_POINTS = findQuartile(dataSet, false);
-
-	const mean: DICE_POINTS = { int: 0, occurs: 0, from: [] };
+	const mean: DICE_POINTS = findMean(dataSet);
 	const upperQuartile: DICE_POINTS = findQuartile(dataSet, true);
 	const maximum: DICE_POINTS = findMaximum(dataSet);
 
@@ -34,7 +33,19 @@ const findQuartile = (data: DICE_DATA_POINT[], loHi: boolean) => {
 	// loHi == true/upper-quartile calc && false/lower-quartile calc
 	const sortedByOccurs = structuredClone(data).sort((a: DICE_DATA_POINT, b: DICE_DATA_POINT) => a.occurs - b.occurs);
 	const quartile = Math.round(data.length / 4);
+	console.log(sortedByOccurs)
 	return loHi ? sortedByOccurs[data.length - quartile] : sortedByOccurs[quartile];
 };
 
-const findMean = (data: DICE_DATA_POINT[]) => {};
+const findMean = (data: DICE_DATA_POINT[]) => {
+	let iterativeSum = 0;
+	data.forEach(datPoint => {
+		iterativeSum += datPoint.occurs;
+	});
+
+	const meanOccurences = iterativeSum / data.length;
+
+	return data.filter(datPoint => {
+		return datPoint.occurs == Math.round(meanOccurences);
+	});
+};
